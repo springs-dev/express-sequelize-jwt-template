d80ad26f7dbfc0f6e8a7800c9e8e0412375081e0
yurii.p <elfuegoyp@gmail.com>
2022-10-25 11:31:28 +0300

    Add SQL syntax highlighting and erd diagram creation

--- config/db.js
+++ config/db.js
@@ -1,11 +1,18 @@
 const Sequelize = require('sequelize');
+const highlightSQL = require('sequelize-log-syntax-colors').default;
 
 module.exports = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: '127.0.0.1',
+    logging:
+      process.env.IS_DB_LOG_ENABLED === 'true'
+        ? (text) => {
+            console.log(highlightSQL(text));
+          }
+        : false,
+    host: process.env.DB_HOST || '127.0.0.1',
     port: 5432,
     dialect: 'postgres',
   }
---
+++ erd.js
@@ -0,0 +1,4 @@
+const db = require('./config/db');
+require('./models');
+
+module.exports = db;
